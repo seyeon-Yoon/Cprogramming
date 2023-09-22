2c026ee6f3c261ee93769ca7819b80bd7659ad41
seyeon-Yoon <108567433+seyeon-Yoon@users.noreply.github.com>
2023-09-22 16:53:01 +0900

    Add files via upload

---
+++ hw1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main() {
+	int num1=10, num2=15;
+	printf("AND결과: %d\n", num1 & num2);
+	printf("OR결과: %d\n", num1 | num2);
+	printf("XOR결과: %d\n", num1 ^ num2);
+	return 0;
+}
